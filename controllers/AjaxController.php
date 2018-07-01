b05e10cd87d1f6d6d8c7c306904e426828ad9aac
Łukasz Micał <lukasz.mical11@gmial.com>
2018-07-01 12:50:50 +0200

    Btc api

--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -2,11 +2,19 @@
 namespace app\controllers;
 
 use base\Controller;
+use models\Ajax;
 
 /**
  * Class AjaxController
  * @package app\controllers
  */
-class AjaxController extends Controller {
+class AjaxController extends Controller 
+{
 
+    function actionBtc() {
+        $model = new Ajax();
+        return $model->getBtcPrices();
+        
+    }
+    
 }
--- models/Ajax.php
+++ models/Ajax.php
@@ -9,5 +9,31 @@ use db\Db;
  */
 class Ajax extends Db {
 
+	public function getBtcPrices(){
+		$url_pln = "https://bitbay.net/API/Public/BTCPLN/trades.json?sort=desc&limit=1";
+		$url_usd = "https://bitbay.net/API/Public/BTCUSD/trades.json?sort=desc&limit=1";
+
+		$data_pln = file_get_contents($url_pln);
+		$data_usd = file_get_contents($url_usd);
+
+		$data_pln = json_decode($data_pln, true);
+		$data_usd = json_decode($data_usd, true);
+
+		$pln = $data_pln[0]["price"];
+		$usd = $data_usd[0]["price"];
+
+		$date = date("d-m-Y H:i:s", $data_pln[0]["date"]);
+		
+
+		return $this->formatPrice($pln)
+		. "|" . $this->formatPrice($usd)
+		. "|" . $date;
+	}
+
+	private function formatPrice($price)
+	{
+		// return $price;
+		return number_format($price, 2);
+	}
 
 }
